903bea083f028448b9bb09636416cf43eb8fb007
Timothee Regis <registimothee@gmail.com>
2020-11-09 16:53:10 +0100

    shorten score method

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+# frozen_string_literal: true
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << ('a'..'z').to_a.sample.upcase
+    end
+    @letters
+  end
+
+  def score
+    @letters_string = params[:grid]
+    @word = params[:word].upcase
+    if included?(@word, @letters_string)
+      results
+      if english_word?(@word)
+      else @result = 'It is not a valid English word!'
+      end
+    else @result = 'The word can’t be built out of the original grid!'
+    end
+  end
+
+  def results
+    @result = 'The word is valid!'
+    @score = @word.length
+    @time = Time.now - params[:time]
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(word, letters_string)
+    word.chars.all? do |letter|
+      word.count(letter) <= letters_string.count(letter)
+    end
+  end
+end
